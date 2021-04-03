59af115e4ca2e437721df88887cc22e95931568b
chengordon <76396427+chengordon@users.noreply.github.com>
2021-04-03 13:08:49 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,21 @@
+let degrees = 0
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+    degrees = input.compassHeading()
+    if (degrees < 45) {
+        basic.showString("N")
+    } else if (degrees < 135) {
+        basic.showString("E")
+    } else if (degrees < 225) {
+        basic.showString("S")
+    } else if (degrees < 315) {
+        basic.showString("W")
+    } else {
+        basic.showString("N")
+    }
+})
